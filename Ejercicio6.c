cc90ccc0f1354bd2b25e8f69b3344915eb0f50d7
F-betanzos <82618274+F-betanzos@users.noreply.github.com>
2021-05-31 19:18:14 -0500

    Update Ejercicio6.c

--- Ejercicio6.c
+++ Ejercicio6.c
@@ -12,20 +12,16 @@ int main()
     banco=0;
     monto=0;
     respuesta ='s';
-
     do
     {
         printf ("\n\n monto a depositar");
         scanf ("%d",&monto);
         banco = banco + monto; //Empieza a trabajar el acumulador//
         numerodedepositos = numerodedepositos + 1; //Trabaja el contador//
-
         printf ("¿Quieres realizar otro deposito (s/n)?");
         scanf ("%s",&respuesta);
-
-
-
-    } while ( respuesta !='s'); //Condicion WHILE significa MIENTRAS//
+   
+     } while ( respuesta !='n'); //Condicion WHILE significa MIENTRAS//
         
             
         printf ("Dinero que tienes en el banco %d \n",banco);
